2f84e21c53d6cca819541d88efe040c65e544880
jeungHunLee <ksjs1111@gmail.com>
2022-08-23 15:45:57 +0900

    string

---
+++ string.js
@@ -0,0 +1,12 @@
+//문자열 출력
+console.log("Hello World!");    //Hello World!
+console.log(typeof("Hello World!"))    //string
+
+//문자열 내부의 따옴표
+console.log("어린왕자가 말했다. \"사막이 아름다운 것은 그것이 어딘가에 우물을 감추고 있기 때문이야.\"")
+
+//줄바꿈 문자 \n
+console.log("어린왕자가 말했다.\n\"사막이 아름다운 것은 그것이 어딘가에 우물을 감추고 있기 때문이야.\"");
+
+//문자열 합치기
+console.log("Hello " + "World!")    //Hello World!
