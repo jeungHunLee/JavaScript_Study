c0cccfb57be82ca50eec86eb0ece0a63fe47a218
jeungHunLee <ksjs1111@gmail.com>
2022-08-26 15:21:55 +0900

    boolean

--- boolean.js
+++ boolean.js
@@ -12,17 +12,17 @@ console.log('a'.charCodeAt());    //97
 console.log('B'.charCodeAt());    //66
 
 //문자 비교
-console.log('a' < 'b')    //true
-console.log('abc' < 10)    //false    'abc'는 숫자로 변환시 NaN, NaN과의 비교는 false
-console.log('0' < true)    //true
+console.log('a' < 'b');    //true
+console.log('abc' < 10);    //false    'abc'는 숫자로 변환시 NaN, NaN과의 비교는 false
+console.log('0' < true);    //true
 
 // ===, !== 연산자
-console.log('1' === 1)    //false    값 비교와 자료형까지 확인
-console.log(1 === true)    //false
-console.log(1 === 1)    //true    값과 자료형이 모두 일치할때 true
-console.log('1' !== 1)    //true
+console.log('1' === 1);    //false    값 비교와 자료형까지 확인
+console.log(1 === true);    //false
+console.log(1 === 1);    //true    값과 자료형이 모두 일치할때 true
+console.log('1' !== 1);    //true
 
 //논리 연산자
-console.log(10 > 5 && 1 < 2)    //true
-console.log(10 < 5 || 1 < 2)    //true
-console.log(!false)    //true
+console.log(10 > 5 && 1 < 2);    //true
+console.log(10 < 5 || 1 < 2);    //true
+console.log(!false);    //true
