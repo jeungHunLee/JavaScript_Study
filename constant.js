43ddc9168c219ffe76213042a419aac555af6487
jeungHunLee <ksjs1111@gmail.com>
2022-08-26 21:15:30 +0900

    constant

---
+++ constant.js
@@ -0,0 +1,11 @@
+//상수 선언
+const pi = 3.14;    //다른 언어의 let을 javascript에서 const로 표현
+//pi = 3.141592;    재정의하면 오류 발생
+//const pi = 3.141592;    재선언하면 오류 발생
+//const myName;    선언과 동시에 초기화하지 않으면 오류 발생
+
+//변수와 비교
+let age = 99;
+console.log(age);    //99
+age = 1;    //let으로 선언된 변수는 재정의 가능
+console.log(age);    //1
