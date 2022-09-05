37281ac91b044b7daf16790023b0deed011c3ed8
jeungHunLee <ksjs1111@gmail.com>
2022-09-05 20:49:07 +0900

    variable

--- variable.js
+++ variable.js
@@ -26,3 +26,35 @@ console.log(hello + friend);    //Hello! 영희
 let number = 5;
 number += 5;
 console.log(number);    //10
+
+//const
+const pi = 3.14;    //다른 언어의 let을 javascript에서 const로 표현
+//pi = 3.141592;    재정의하면 오류 발생
+//const pi = 3.141592;    재선언하면 오류 발생
+//const myName;    선언과 동시에 초기화하지 않으면 오류 발생
+
+//var hoisting
+console.log(a);    //undefined
+
+var a = 2;
+console.log(a);    //2
+
+test();    //var 타입 hoisting
+
+function test() {
+    console.log("var 타입 hoisting");
+}
+
+//let, const hoisting
+//console.log(b);    선언 후 초기화는 안하기 때문에 에러
+
+let b = 5;
+console.log(b);    //5
+
+//test();
+
+var test = function() {
+    console.log("let, const 타입 hoisting");
+}
+
+test();
