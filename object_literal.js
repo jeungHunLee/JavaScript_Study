41b4a4bb1e05bd426b37ad248f888b0141739c24
jeungHunLee <ksjs1111@gmail.com>
2022-09-19 21:33:24 +0900

    object literal

---
+++ object_literal.js
@@ -0,0 +1,41 @@
+//객체 리터럴
+const person = {
+    name: '홍길동',
+    age: 99,
+    gender: 'man',
+    say: function() {    //method
+        console.log("Hello World!")
+    },
+};
+
+console.log(person.name);    //홍길동
+console.log(person['name']);    //홍길동
+console.log(person.age);    //99
+console.log(person['age']);    //99
+
+delete person.gender;
+console.log(person.gender);    //undefined
+
+person.say();    //Hello World!
+
+//객체의 참조
+const a = {name: "hong",};
+const b = a;
+a.name = "Kim";
+console.log(b.name);    //Kim
+
+let c = "hello";
+let d = c;    //copy
+c = "hi";
+console.log(d);    //hello
+
+//객체 literal 내부 속성을 가리키는 this
+let object = {
+    name: "철수",
+    age: 99,
+    say: function() {
+        console.log("저의 이름은 " + this.name + "이고, 나이는" + this.age + "입니다.");
+    }
+}
+
+object.say()
